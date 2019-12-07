5d9cc6704e872022306b8ed5f29d6be884d0109d
i use arch btw <sob@is-MacBook-Pro.local>
2019-12-07 10:44:24 +0200

    scale down option

--- main.cpp
+++ main.cpp
@@ -12,13 +12,20 @@ int main (const int argc, const char * argv[])
   const char* filename = cimg_option("-i","image.jpg","Input image file");
   const int height = cimg_option("-h",480,"desired height of the output image");
   const int width = cimg_option("-w",640,"desired width of the output image");
+  const double scale = cimg_option("-s",0.5,"scale down the image with desired modifier");
   const char* newfile = cimg_option("-o","image_new.jpg","output image file");
   CImg<unsigned char> img(filename);
-  if (img)
-    cout<<"i think it works not sure"<<endl;
-  img.resize(width, height);
-  if (img)
-    cout<<"i think it works not sure"<<endl;
-  img.save(newfile);
+  if(scale)
+    {
+      int width_s = img.width()*scale;
+      int height_s = img.height()*scale;
+      img.resize(width_s, height_s);
+      img.save(newfile);
+    }
+  else
+    {
+    img.resize(width, height);
+    img.save(newfile);
+    }
   return 0;
 }
