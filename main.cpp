88f5462f8df0462ada72447db68fab4fa0c0ebba
Azimov Parviz <parviz.azimov@student.lamk.fi>
2019-12-22 18:19:52 +0200

    fixed scaledown bug

--- main.cpp
+++ main.cpp
@@ -11,25 +11,26 @@ int main (const int argc, const char * argv[])
   const char* filename = cimg_option("-i","image.jpg","Input image file");
   const int height = cimg_option("-h",0,"desired height of the output image");
   const int width = cimg_option("-w",0,"desired width of the output image");
-  const double scale = cimg_option("-s",0.5,"scale down the image with desired modifier");
+  const double scale = cimg_option("-s",0.0,"scale down the image with desired modifier");
   const char* newfile = cimg_option("-o","image_new.jpg","output image file");
   //creating cimg object
   CImg<unsigned char> img(filename);
   
+  int width_s = img.width()*scale;
+  int height_s = img.height()*scale;
+  
   //scaledown options
   if(scale)
     {
-      int width_s = img.width()*scale;
-      int height_s = img.height()*scale;
       img.resize(width_s, height_s);
       img.save(newfile);
     }
   
   //given width and height
-  else if(width &  height)
+  if(width &  height)
     {
-    img.resize(width, height);
-    img.save(newfile);
+      img.resize(width, height);
+      img.save(newfile);
     }
   else if(width)
     {
