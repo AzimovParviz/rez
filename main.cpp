6f34bffc33e5b946fd0d3a3d7e368c72bed8415f
Azimov Parviz <parviz.azimov@student.lamk.fi>
2019-12-25 16:06:28 +0200

    tabulation and gitignore for emacs fiels

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main (const int argc, const char * argv[])
   const char* filename = cimg_option("-i","image.jpg","Input image file");
   const int height = cimg_option("-h",0,"desired height of the output image");
   const int width = cimg_option("-w",0,"desired width of the output image");
-  const double scale = cimg_option("-s",0.0,"scale down the image with desired modifier");
+  const double scale = cimg_option("-s",0.0,"scale the image with desired modifier");
   const int color = cimg_option("-c", 0,"choose color scheme, 0 - unchanged, 1 - black and white");
   const char* newfile = cimg_option("-o","image_new.jpg","output image file");
   //creating cimg object
@@ -23,7 +23,7 @@ int main (const int argc, const char * argv[])
   int width_s = img.width()*scale;
   int height_s = img.height()*scale;
   
-  //scaledown options
+  //scaling options
   if(scale)
     {
       switch(color)
@@ -46,9 +46,33 @@ int main (const int argc, const char * argv[])
   //given width and height
   if(width & height)
     {
-      switch(color){
-	
-      case 0:
+      switch(color)
+	{	
+	case 0:
+	  if(width<img.width() && height<img.height())
+	    {
+	      img.resize(width, height, 1, img.spectrum(), 3);
+	      img.save(newfile);
+	    }	
+	else if(width>=img.width() || height>=img.height())
+	  {
+	    img.resize(width, height, 1, img.spectrum(), 6);
+	    img.save(newfile);
+	  }
+	break;	
+	case 1://b&w
+	  if(width<img.width() && height<img.height())
+	    {
+	      img.resize(width, height, 1, 1, 3);
+	      img.save(newfile);
+	    }	
+	else if(width>=img.width() || height>=img.height())
+	  {
+	    img.resize(width, height, 1, 1, 6);
+	    img.save(newfile);
+	  }      
+	  break;      	
+      default:
 	if(width<img.width() && height<img.height())
 	  {
 	    img.resize(width, height, 1, img.spectrum(), 3);
@@ -59,36 +83,10 @@ int main (const int argc, const char * argv[])
 	    img.resize(width, height, 1, img.spectrum(), 6);
 	    img.save(newfile);
 	  }
-	break;
-	
-      case 1://b&w
-	if(width<img.width() && height<img.height())
-	  {
-	    img.resize(width, height, 1, 1, 3);
-	    img.save(newfile);
-	  }	
-	else if(width>=img.width() || height>=img.height())
-	{
-	  img.resize(width, height, 1, 1, 6);
-	  img.save(newfile);
-	}      
-	break;      
-	
-      default:
-	if(width<img.width() && height<img.height())
-	  {
-	    img.resize(width, height, 1, img.spectrum(), 3);
-	    img.save(newfile);
-	  }	
-	else if(width>=img.width() || height>=img.height())
-	{
-	  img.resize(width, height, 1, img.spectrum(), 6);
-	  img.save(newfile);
 	}
-      }
     } 
   
-
+  
   //if only width is provided
   else if(width)
     {
@@ -102,7 +100,7 @@ int main (const int argc, const char * argv[])
 	else if(width>img.width())
 	  {
 	    img.resize(width, img.height(), 1, img.spectrum(), 6);
-	  img.save(newfile);
+	    img.save(newfile);
 	  }
 	break;
       case 1://black&white
@@ -131,7 +129,7 @@ int main (const int argc, const char * argv[])
 	break;
       }
     }
-
+  
   //if only height is provided
   else if(height)
     {
