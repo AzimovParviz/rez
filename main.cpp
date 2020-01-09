51d9fc6378889ae9f659ba94ba4a1763a317c74b
Parviz Azimov <parviz.azimov@student.lamk.fi>
2020-01-09 23:55:41 +0300

    minor changes

--- main.cpp
+++ main.cpp
@@ -12,18 +12,17 @@ int main (const int argc, const char * argv[])
   //options
   cimg_usage("Retrieve command line arguments");
   const char* filename = cimg_option("-i","image.jpg","Input image file");
-  CImg<unsigned char> img(filename);
+  CImg<unsigned char> img(filename);  //creating cimg object
   const int height = cimg_option("-h",img.height(),"desired height of the output image");
   const int width = cimg_option("-w",img.width(),"desired width of the output image");
   const double scale = cimg_option("-s",1.0,"scale the image with desired modifier");
   const int rotate = cimg_option("-r",0,"rotate the image INT degrees");
   const int color = cimg_option("-c", img.spectrum(),"choose color scheme, default - unchanged, 1 - black and white, 3 - RGB, 4 - RGBA");
   const char* newfile = cimg_option("-o","image_new.jpg","output image file");
-  //creating cimg object
-  cout<<height<<" "<<width<<"h and w of the image kek"<<endl;
+  cout<<width<<" "<<height<<" width and height of the image"<<endl;
   if(rotate)
     img.rotate(rotate, 2);
-  /* setting up interpolation type based on whenever the image is upscaled or downscaled*/
+  /* setting up interpolation type (6 - cubic, 3 - linear) based on whenever the image is upscaled or downscaled*/
   if(height<img.height() || width<img.width() || scale<1.0)
     img.resize(width*scale,height*scale, 1, color, 6);
   else
